7b886456bcc877826f73c5c84f98b782abb0556a
Ranbir Aulakh <ranbiraulakhme@gmail.com>
2023-10-30 13:46:32 +0000

    Fixed the threading issue

--- src/aws/osml/tile_server/viewpoint/queue.py
+++ src/aws/osml/tile_server/viewpoint/queue.py
@@ -1,14 +1,12 @@
-
-import logging
-from typing import Any, Dict
 import json
-import boto3
-from aws.osml.tile_server.app_config import ServerConfig, BotoConfig
+import logging
+from typing import Dict
+
 from botocore.exceptions import ClientError
-from boto3.resources.base import ServiceResource
+
 
 class ViewpointRequestQueue:
-    def __init__(self, aws_sqs, queue_name: str):
+    def __init__(self, aws_sqs, queue_name: str) -> None:
         self.sqs_client = aws_sqs
         self.queue = self.sqs_client.get_queue_by_name(QueueName=queue_name)
         self.logger = logging.getLogger("uvicorn")
@@ -25,16 +23,14 @@ class ViewpointRequestQueue:
         except ClientError as error:
             self.logger.error("Unable to send message visibility: {}".format(error))
 
-    def finish_request(self, receipt_handle: str):
+    def finish_request(self, receipt_handle: str) -> None:
         """
         Delete the message from the SQS
-        
+
         :param receipt_handle: str = unique identifier of a message
         :return: None
         """
         try:
             self.queue.delete_message(ReceiptHandle=receipt_handle)
-            # self.sqs_client.delete_message(QueueUrl=self.queue_url, ReceiptHandle=receipt_handle)
         except ClientError as error:
             self.logger.error("Unable to remove message from queue: {}".format(error))
-
--- src/aws/osml/tile_server/viewpoint/worker.py
+++ src/aws/osml/tile_server/viewpoint/worker.py
@@ -1,21 +1,27 @@
-import asyncio
-import io
+import json
 import logging
 import os
-import shutil
 from pathlib import Path
-from botocore.exceptions import ClientError
-import json
+from time import sleep
+import asyncio
 
-from .models import ViewpointStatus, ViewpointModel
-from .database import DecimalEncoder, ViewpointStatusTable
-from .queue import ViewpointRequestQueue
 from boto3.resources.base import ServiceResource
+from botocore.exceptions import ClientError
+
+from .database import DecimalEncoder, ViewpointStatusTable
+from .models import ViewpointModel, ViewpointStatus
+from .queue import ViewpointRequestQueue
 
 FILESYSTEM_CACHE_ROOT = os.getenv("VIEWPOINT_FILESYSTEM_CACHE", "/tmp/viewpoint")
 
-class ViewpointWorker():
-    def __init__(self, viewpoint_request_queue: ViewpointRequestQueue, aws_s3: ServiceResource, viewpoint_database: ViewpointStatusTable):
+
+class ViewpointWorker:
+    def __init__(
+        self,
+        viewpoint_request_queue: ViewpointRequestQueue,
+        aws_s3: ServiceResource,
+        viewpoint_database: ViewpointStatusTable,
+    ):
         self.viewpoint_request_queue = viewpoint_request_queue
         self.s3 = aws_s3
         self.viewpoint_database = viewpoint_database
@@ -23,9 +29,10 @@ class ViewpointWorker():
 
     async def run(self) -> None:
         """
-        Monitors SQS queues for ViewpointRequest and be able to process it. First, it will pick up a message from ViewpointRequest SQS.
-        Then, it will download an image from S3 and save it to the local temp directory. Once that's completed, it will update the DynamoDB 
-        to reflect that this Viewpoint is READY to review. This function will run in the background. 
+        Monitors SQS queues for ViewpointRequest and be able to process it. First, it will
+        pick up a message from ViewpointRequest SQS. Then, it will download an image from S3
+        and save it to the local temp directory. Once that's completed, it will update the DynamoDB
+        to reflect that this Viewpoint is READY to review. This function will run in the background.
 
         :return: None
         """
@@ -34,14 +41,14 @@ class ViewpointWorker():
             try:
                 messages = self.viewpoint_request_queue.queue.receive_messages()
                 if len(messages) == 0:
-                    sleep(5) # sleep for 5 seconds and retry
+                    await asyncio.sleep(5)  # sleep for 5 seconds and retry
                 for message in messages:
                     self.logger.info(f"MESSAGE: {message.body}")
                     message_attributes = json.loads(message.body)
                     viewpoint_item = ViewpointModel.model_validate_json(json.dumps(message_attributes, cls=DecimalEncoder))
 
                     viewpoint_new_status = ViewpointStatus.REQUESTED
-                    
+
                     message_viewpoint_id = viewpoint_item.viewpoint_id
                     message_viewpoint_status = viewpoint_item.viewpoint_status
                     message_object_key = viewpoint_item.object_key
@@ -56,15 +63,17 @@ class ViewpointWorker():
                         # download file to temp local (TODO update when efs is implemented)
                         retry_count = 0
                         internal_err = None
-                        while(retry_count < 3):
+                        while retry_count < 3:
                             try:
                                 self.s3.meta.client.download_file(
                                     message_bucket_name, message_object_key, str(local_object_path.absolute())
                                 )
-                                
+
                                 viewpoint_new_status = ViewpointStatus.READY
-                                self.logger.info(f"Successfully download {message_bucket_name}/{message_object_key} to {str(local_object_path.absolute())}. This Viewpoint is now READY!")
-                                
+                                self.logger.info(
+                                    f"Successfully download to {str(local_object_path.absolute())}. This Viewpoint is now READY!"
+                                )
+
                                 internal_err = None
 
                                 break
@@ -75,7 +84,9 @@ class ViewpointWorker():
                                     self.logger.error(internal_err)
 
                                 elif err.response["Error"]["Code"] == "403":
-                                    internal_err = f"You do not have permission to access {message_bucket_name} bucket! Error={err}"
+                                    internal_err = (
+                                        f"You do not have permission to access {message_bucket_name} bucket! Error={err}"
+                                    )
                                     self.logger.error(internal_err)
 
                                 internal_err = f"Image Tile Server cannot process your S3 request! Error={err}"
@@ -85,7 +96,7 @@ class ViewpointWorker():
                                 internal_err = f"Something went wrong! Viewpoint_id: {message_viewpoint_id}! Error={err}"
                                 self.logger.error(internal_err)
                                 viewpoint_new_status = ViewpointStatus.FAILED
-                            
+
                             retry_count += 1
 
                         # update ddb
@@ -100,7 +111,9 @@ class ViewpointWorker():
                         # remove message from the queue since it has been processed
                         message.delete()
                     else:
-                        self.logger.error(f"Cannot process this viewpoint request - {message_viewpoint_id} due to the incorrect Viewpoint Status {message_viewpoint_status}!")
+                        self.logger.error(
+                            f"Cannot process {message_viewpoint_id} due to the incorrect Viewpoint Status {message_viewpoint_status}!"
+                        )
                         continue
 
             except ClientError as err:
@@ -110,4 +123,4 @@ class ViewpointWorker():
             except Exception as err:
                 self.logger.warning(f"[Worker Background Thread] {err}")
 
-            sleep(5)
+            await asyncio.sleep(5)
