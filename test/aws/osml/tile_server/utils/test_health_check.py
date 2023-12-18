60d16cdbe16dbfac870a8532ec6c46facfa656b4
jtblack <jtblack@amazon.com>
2023-12-18 16:53:41 -0700

    fixing tests

---
+++ test/aws/osml/tile_server/utils/test_health_check.py
@@ -0,0 +1,11 @@
+#  Copyright 2023 Amazon.com, Inc. or its affiliates.
+
+from unittest import TestCase
+
+from aws.osml.tile_server.utils import HealthCheck
+
+
+class TestHealthCheck(TestCase):
+    def test_health_check(self):
+        health_check = HealthCheck(status="OK")
+        assert health_check.status == "OK"
