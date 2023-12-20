96ec4b9ab82871bd2b240437b998c67e9109eaaf
drduhe <drduhe@amazon.com>
2023-12-20 11:04:02 -0700

    fix: updating pydoc comments throughout package (#28)
    Updating pydoc comments throughout package

--- src/aws/osml/tile_server/viewpoint/models.py
+++ src/aws/osml/tile_server/viewpoint/models.py
@@ -44,7 +44,7 @@ class ViewpointStatus(str, AutoUnderscoreStringEnum):
 
     These states represent the state of processing or readiness for a viewpoint.
 
-    :cvar NOT_FOUND: Viewpoint not found.
+    :cvar NOT_FOUND: Viewpoint isn't found.
     :cvar REQUESTED: Viewpoint requested.
     :cvar UPDATING: Viewpoint updating.
     :cvar READY: Viewpoint ready.
@@ -64,11 +64,13 @@ class ViewpointRequest(BaseModel):
     """
     Represents a request to create or update a viewpoint.
 
-    :param viewpoint_name: The name of the viewpoint. Minimum length is 1.
-    :param bucket_name: The name of the bucket. Minimum length is 1.
-    :param object_key: The key of the object. Minimum length is 1.
-    :param tile_size: The tile size. Should be greater than 0.
-    :param range_adjustment: The range adjustment type. Minimum string length is 1.
+    All fields are required.
+
+    :param bucket_name: The name of the bucket, the Minimum length is 1.
+    :param object_key: The key of the object, the Minimum length is 1.
+    :param viewpoint_name: The name of the viewpoint, the Minimum length is 1.
+    :param tile_size: The tile size, it Should be greater than 0.
+    :param range_adjustment: The range adjustment type, the Minimum string length is 1.
     """
 
     viewpoint_name: str = Field(min_length=1)
@@ -83,16 +85,16 @@ class ViewpointModel(BaseModel):
     Represents the model data for a viewpoint, including its ID, name, status, bucket name, object key, tile size,
     range adjustment, local object path, and error message.
 
-    :param viewpoint_id: The ID of the viewpoint
+    :param viewpoint_id: The ID to associate with the viewpoint.
     :param viewpoint_name: The name of the viewpoint.
     :param viewpoint_status: The status of the viewpoint.
-    :param bucket_name: The name of the bucket.
-    :param object_key: The object key.
-    :param tile_size: The tile size.
+    :param bucket_name: The name of the bucket the images are located in.
+    :param object_key: The object is key to associate with the viewpoint.
+    :param tile_size: The tile size to use for the viewpoint model.
     :param range_adjustment: The type of range adjustment.
-    :param local_object_path: The local path of the object. May be None.
-    :param error_message: Any error messages. May be None.
-    :param expire_time: The expiration time. May be None.
+    :param local_object_path: Optional local path of the object.
+    :param error_message: Optional error messages to include.
+    :param expire_time: Optional expiration time for the message.
     """
 
     viewpoint_id: str
