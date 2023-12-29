6a7526ce191638e89bad22541d9b90a13d279548
Ranbir Aulakh <ranbiraulakhme@gmail.com>
2023-12-29 15:32:24 +0000

    fix: Remove pickle based on ASH findings

--- src/aws/osml/tile_server/utils/token.py
+++ src/aws/osml/tile_server/utils/token.py
@@ -1,7 +1,9 @@
 #  Copyright 2023 Amazon.com, Inc. or its affiliates.
 
-import pickle
+import json
+import logging
 from os import path
+from typing import Optional
 
 from cryptography.fernet import Fernet
 
@@ -9,13 +11,34 @@ from ..app_config import ServerConfig
 
 TS_TOKEN_FILE_NAME = "ts-token"
 
+logger = logging.getLogger("uvicorn")
+
 
 def initialize_token_key() -> None:
+    """
+    Initializes and stores a new encryption token key in a JSON file if one does not already exist.
+
+    :return: None
+    """
     file_path = path.join(f"/{ServerConfig.efs_mount_name}", TS_TOKEN_FILE_NAME)
     if not path.isfile(file_path):
-        pickle.dump(Fernet.generate_key(), open(file_path, "wb"))
+        key = Fernet.generate_key()
+        with open(file_path, "w") as token_file:  # Change to "w" for writing text.
+            json.dump({"token": key.decode()}, token_file)  # Store as JSON.
 
 
-def read_token_key() -> bytes:
+def read_token_key() -> Optional[bytes]:
+    """
+    Reads and returns the encryption token key from a JSON file.
+
+    :return: The encryption token key as a dictionary if the file exists and contains valid JSON, None otherwise.
+    """
     file_path = path.join(f"/{ServerConfig.efs_mount_name}", TS_TOKEN_FILE_NAME)
-    return pickle.load(open(file_path, "rb"))
+    try:
+        with open(file_path, "r") as token_file:
+            token: str = json.load(token_file).get("token")
+            return token.encode()
+    except Exception as err:
+        # Handle the case where the file is empty or not valid JSON
+        logger.error(f"Not able to token key from json file {file_path} wth error: {err}")
+        return None
